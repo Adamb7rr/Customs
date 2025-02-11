bf00cf1a9b81258288d49926d3b9d85514a6fb78
Adamb7rr <bahrcr7@gmail.com>
2025-02-11 21:19:10 +0200

    Added

--- Memory-Blocks-Game/main.js
+++ Memory-Blocks-Game/main.js
@@ -1,13 +1,25 @@
-
-
+// splach screen
 document.querySelector('.control-buttons span').onclick = () => {
+    // prompt to enter your name
     let yourName = prompt("Enter Your Name?")
-    console.log(yourName)
+
+    // display your name in game page
     if (yourName === '') {
         document.querySelector('.name span').innerHTML = `Unknown`
     } else {
         document.querySelector('.name span').innerHTML = `${yourName}`
     }
 
+    // remove splach screen from page
     document.querySelector('.control-buttons').remove()
 }
+
+let duration = 1000
+
+let blocksContainer = document.querySelector('.memory-game-blocks')
+
+let blocks = Array.from(blocksContainer.children)
+
+let orderRange = [...Array(blocks.length).keys()]
+
+console.log(orderRange)
