969ab501d18cf6616c70177c4427ce2ff4187011
Adamb7rr <bahrcr7@gmail.com>
2025-02-11 21:52:51 +0200

    Added

--- Memory-Blocks-Game/main.js
+++ Memory-Blocks-Game/main.js
@@ -20,8 +20,32 @@ let blocksContainer = document.querySelector('.memory-game-blocks')
 
 let blocks = Array.from(blocksContainer.children)
 
-let orderRange = [...Array(blocks.length).keys()]
+let orderRange = Array.from(Array(blocks.length).keys())
+
+console.log(orderRange)
+shuffle(orderRange)
+console.log(orderRange)
 
 blocks.forEach((block, index) => {
-    block.style.order = index
+    block.style.order = orderRange[index]
 })
+
+function shuffle (array) {
+    let current = array.length,
+        temp,
+        random;
+
+    while (current > 0) {
+        random = Math.floor(Math.random() * current)
+
+        current--
+
+        temp = array[current]
+
+        array[current] = array[random]
+
+        array[random] = temp
+    }
+
+    return array
+}
