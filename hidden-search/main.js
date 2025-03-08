cefb8aad67a55dd9f7c05c3cc110b2cce76f5c4f
Adamb7rr <bahrcr7@gmail.com>
2025-03-08 08:33:54 +0200

    update

--- hidden-search/main.js
+++ hidden-search/main.js
@@ -1,2 +1,8 @@
-const butSearch = document.querySelector('div')
+const search = document.querySelector(".search");
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".input");
 
+btn.addEventListener("click", () => {
+    search.classList.toggle("active");
+    input.focus();
+});
